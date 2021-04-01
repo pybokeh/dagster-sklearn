80fa387b583174a9f79e14c2ffd639613da7ce4f
pybokeh <pybokeh@protonmail.com>
2021-04-01 18:37:34 -0400

    Added schedule to execute pipeline every weekday at 9am

--- src/repositories/sklearn_repository.py
+++ src/repositories/sklearn_repository.py
@@ -1,5 +1,6 @@
 from dagster import repository
 from pipelines.sklearn_pipeline import sklearn_pipeline
+from schedules.sklearn_schedule import every_weekday_9am
 
 
 @repository
@@ -8,4 +9,7 @@ def sklearn_repo():
         "pipelines": {
             "sklearn_pipeline": lambda: sklearn_pipeline 
         },
+        "schedules": {
+            "every_weekday_9am": lambda: every_weekday_9am
+        }
     }
