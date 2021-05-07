e12c4a95e65a189c19ada403ace8db260507e4ba
pybokeh <pybokeh@protonmail.com>
2021-05-07 19:52:04 -0400

    Shortened and clarified reason for using lazy form

--- src/repositories/sklearn_repository.py
+++ src/repositories/sklearn_repository.py
@@ -3,9 +3,10 @@ from pipelines.sklearn_pipeline import sklearn_pipeline
 from schedules.sklearn_schedule import every_weekday_9am
 
 # There are 2 forms of return value for the repository definition:
-# list or dict form (for "lazy" evaluation)
-# Even though not needed, I personally prefer the dict form as I
-# prefer explicitness as the key value aids in describing that
+# 1) list or 
+# 2) dict form (for "lazy" evaluation)
+# Even though not needed in this example, I personally prefer the dict
+# form for the explicitness it provides as the key value aids in describing that
 # I am passing in a pipeline vs schedule vs sensor, etc
 # Documentation reference:
 # https://docs.dagster.io/_apidocs/repositories#dagster.RepositoryDefinition
