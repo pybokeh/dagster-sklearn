b93687f15b4eee7a9eda3b3fe8546f000242f989
pybokeh <pybokeh@protonmail.com>
2021-05-07 19:48:44 -0400

    Added why I prefer dict form for repository return type

--- src/repositories/sklearn_repository.py
+++ src/repositories/sklearn_repository.py
@@ -2,6 +2,13 @@ from dagster import repository
 from pipelines.sklearn_pipeline import sklearn_pipeline
 from schedules.sklearn_schedule import every_weekday_9am
 
+# There are 2 forms of return value for the repository definition:
+# list or dict form (for "lazy" evaluation)
+# Even though not needed, I personally prefer the dict form as I
+# prefer explicitness as the key value aids in describing that
+# I am passing in a pipeline vs schedule vs sensor, etc
+# Documentation reference:
+# https://docs.dagster.io/_apidocs/repositories#dagster.RepositoryDefinition
 
 @repository
 def sklearn_repo():
